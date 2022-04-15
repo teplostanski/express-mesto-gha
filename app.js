483c04b53389d20958fd41525f4247ec2ea1fbfc
r0bomurlok <igrtnsk@ya.ru>
2022-04-15 00:34:16 +0000

    fix: mongodb

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(requestLogger); // подключаем логгер запросов
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 }, (err) => {
   if (err) {
