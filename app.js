d805a345a199f806de4fee92969c1f5cbf69800f
r0bomurlok <igrtnsk@ya.ru>
2022-04-16 16:07:57 +0300

    crush test

--- app.js
+++ app.js
@@ -33,6 +33,13 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 });
 
 app.use(requestLogger); // подключаем логгер запросов
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post(
   '/signin',
   celebrate({
